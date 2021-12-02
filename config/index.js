2394dc1eb99c636074d8d279568e93d924ab222c
Jimmy1186 <sususu089@gmail.com>
2021-12-02 13:08:52 +0800

    domain fix3

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://portfolio-azf9m7zfy-jimmy1186.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://portfolio-pearl-zeta-68.vercel.app/';
