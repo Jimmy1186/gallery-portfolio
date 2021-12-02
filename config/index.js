d12b4a0c6a2ad4474bfb51bb910085fe8375480b
Jimmy1186 <sususu089@gmail.com>
2021-12-02 16:02:09 +0800

    domain fix10

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://gallery-portfolio-6e9ch39t4-jimmy1186.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://gallery-portfolio.vercel.app.app';
